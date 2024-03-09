9013e5be212cbcba3c7defcc62a4f6ef864f0a1f
smehlhoff <stevemehlhoff@gmail.com>
2024-03-09 09:18:59 -0800

    Update models

--- core/models.py
+++ core/models.py
@@ -1,7 +1,18 @@
 import os
 
-from sqlalchemy import URL, Boolean, Column, Float, ForeignKey, Integer, String, create_engine
+from sqlalchemy import (
+    URL,
+    Boolean,
+    Column,
+    DateTime,
+    Float,
+    ForeignKey,
+    Integer,
+    String,
+    create_engine,
+)
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.sql import func
 
 conn = URL.create(
     "postgresql",
@@ -21,6 +32,7 @@ class Station(Base):
     __tablename__ = "stations"
 
     id = Column(Integer, primary_key=True)
+
     station_code = Column(String)
     lat = Column(Float)
     lon = Column(Float)
@@ -29,6 +41,9 @@ class Station(Base):
     state = Column(String)
     country = Column(String)
 
+    create_date = Column(DateTime, default=func.now())
+    update_date = Column(DateTime, default=func.now(), onupdate=func.now())
+
     metars = relationship("Metar", back_populates="station")
 
 
@@ -36,23 +51,31 @@ class Metar(Base):
     __tablename__ = "metars"
 
     id = Column(Integer, primary_key=True)
+
     station_id = Column(Integer, ForeignKey("stations.id"))
+    station = relationship("Station", back_populates="metars")
+
     raw_text = Column(String)
     station_code = Column(String)
+
     observation_time = Column(String)
+
     temp_c = Column(Float, nullable=True)
     temp_f = Column(Float, nullable=True)
     dewpoint_c = Column(Float, nullable=True)
     dewpoint_f = Column(Float, nullable=True)
+
     wind_dir_degrees = Column(String, nullable=True)
     wind_dir_cardinal = Column(String, nullable=True)
     wind_speed_kt = Column(Integer, nullable=True)
     wind_speed_mph = Column(Integer, nullable=True)
     wind_gust_kt = Column(Integer, nullable=True)
     wind_gust_mph = Column(Integer, nullable=True)
+
     visibility_statute_mi = Column(Float, nullable=True)
     altim_in_hg = Column(Float, nullable=True)
     sea_level_pressure_mb = Column(Float, nullable=True)
+
     corrected = Column(Boolean)
     auto = Column(Boolean)
     auto_station = Column(Boolean)
@@ -61,6 +84,7 @@ class Metar(Base):
     lightning_sensor_off = Column(Boolean)
     freezing_rain_sensor_off = Column(Boolean)
     present_weather_sensor_off = Column(Boolean)
+
     wx_string = Column(String, nullable=True)
     sky_cover = Column(String, nullable=True)
     cloud_base_ft_agl = Column(Integer, nullable=True)
@@ -72,6 +96,7 @@ class Metar(Base):
     cloud_base_ft_agl_6 = Column(Integer, nullable=True)
     flight_category = Column(String, nullable=True)
     three_hr_pressure_tendency_mb = Column(Float, nullable=True)
+
     maxt_c = Column(Float, nullable=True)
     maxt_f = Column(Float, nullable=True)
     mint_c = Column(Float, nullable=True)
@@ -80,15 +105,19 @@ class Metar(Base):
     maxt24hr_f = Column(Float, nullable=True)
     mint24hr_c = Column(Float, nullable=True)
     mint24hr_f = Column(Float, nullable=True)
+
     precip_in = Column(Float, nullable=True)
     pcp3hr_in = Column(Float, nullable=True)
     pcp6hr_in = Column(Float, nullable=True)
     pcp24hr_in = Column(Float, nullable=True)
     snow_in = Column(Float, nullable=True)
+
     vert_vis_ft = Column(Integer, nullable=True)
+
     metar_type = Column(String, nullable=True)
 
-    station = relationship("Station", back_populates="metars")
+    create_date = Column(DateTime, default=func.now())
+    update_date = Column(DateTime, default=func.now(), onupdate=func.now())
 
 
 Base.metadata.create_all(engine)
